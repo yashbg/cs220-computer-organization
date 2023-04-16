76bb3fc65311572e4376c9566a527ed374ba9013
Yash Gupta <yashbg7@gmail.com>
2023-04-16 15:31:48 +0530

    Correct lw

--- assignment-7/bubble_sort.s
+++ assignment-7/bubble_sort.s
@@ -7,13 +7,14 @@ n: .word 5
 
 main:
     la $s0, arr                         # $s0 = arr
-    lw $s1, n                           # $s1 = n
+    la $s4, n                           # $s4 = &n
+    lw $s1, 0($s4)                      # $s1 = n
 
     li $s2, 0                           # $s2 = i (counter for outer_loop)
 
 outer_loop:
     addi $t0, $s1, -1                   # $t0 = n - 1
-    bgte $s2, $t0, end          # if i >= n - 1, end outer_loop
+    bgte $s2, $t0, end                  # if i >= n - 1, end outer_loop
 
     li $s3, 0                           # $s3 = j (counter for inner_loop)
 
