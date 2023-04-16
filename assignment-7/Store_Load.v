b7704786a413cc1f88b664a6569a6611d3202cb1
Yash Gupta <yashbg7@gmail.com>
2023-04-16 15:15:48 +0530

    Fill data memory

--- assignment-7/Store_Load.v
+++ assignment-7/Store_Load.v
@@ -26,27 +26,12 @@ module Store_Load(
 		for (i=0; i < 1024; i++) begin
       Data_Memory[i] = 0;
 		end
-    Data_Memory[0] = 32'd20;
-    Data_Memory[1] = 32'd5;
-    Data_Memory[2] = 32'd2;
-    Data_Memory[3] = 32'd7;
-    Data_Memory[4] = 32'd11;
-    Data_Memory[5] = 32'd9;
-    Data_Memory[6] = 32'd4;
-    Data_Memory[7] = 32'd15;
-    Data_Memory[8] = 32'd0;
-    Data_Memory[9] = 32'd12;
-    Data_Memory[10] = 32'd3;
-    Data_Memory[11] = 32'd5;
-    Data_Memory[12] = 32'd2;
-    Data_Memory[13] = 32'd7;
-    Data_Memory[14] = 32'd11;
-    Data_Memory[15] = 32'd13;
-    Data_Memory[16] = 32'd24;
-    Data_Memory[17] = 32'd12;
-    Data_Memory[18] = 32'd17;
-    Data_Memory[19] = 32'd15;
-    Data_Memory[20] = 32'd4;
+    Data_Memory[0] = 32'd7;
+    Data_Memory[1] = 32'd3;
+    Data_Memory[2] = 32'd8;
+    Data_Memory[3] = 32'd4;
+    Data_Memory[4] = 32'd2;
+    Data_Memory[5] = 32'd5;
 	end
 
 
